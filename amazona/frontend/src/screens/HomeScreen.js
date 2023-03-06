d6444aabcb5a1fdc5d8d9a0a750ae233f249fce1
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2023-03-06 12:35:37 -0500

    installed react-bootstrap, in app.js created navbar, in index.js imported bootstrap, in index.html imported fontawesome, index.css set class to primary for btn and changed rating to gold color, created two new components product.js to render product itmes in the list and rating.js to render rating based on five stars, homscreen.js changed product list to bootstrap

--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,75 +1,71 @@
 import React, { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import logger from 'use-reducer-logger';
+import axios from "axios";
+import logger from "use-reducer-logger";
+// import {Row, Col} from 'react-bootstrap';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Product } from "../components/Product";
 // import data from "../data";
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'FETCH_REQUEST':
-            // true will allow a loading box to show
-            return { ...state, loading: true };
-        case 'FETCH_SUCCESS':
-            // action.payload contains all products from backend
-            return { ...state, products: action.payload, loading: false };
-        case 'FETCH_FAIL':
-            // returns previous state and returns error
-            return { ...state, loading: false, error: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      // true will allow a loading box to show
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      // action.payload contains all products from backend
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      // returns previous state and returns error
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
 };
 
 function HomeScreen() {
-    // commented out userState and using reducer as it is more reliable 
-    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
-        products: [],
-        loading: true,
-        error: '',
-    });
-    // const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            dispatch({ type: 'FETCH_REQUEST' });
-            try {
-                const result = await axios.get('/api/products');
-                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
-            } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: err.message });
-            }
-            // setProducts(result.data);
-        };
-        fetchData();
-    }, []);
+  // commented out userState and using reducer as it is more reliable
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
 
-    return (<div>
-        <h1>Featured Products</h1>
-        <div className="products">
-            {/* mapping data from the data.js in the backend */}
-            {
-                loading ? <div>Loading...</div>
-                    :
-                    error ? <div>{error}</div>
-                        :
-                        products.map((product) => (
-                            <div className="product" key={product.slug}>
-                                <Link to={`/product/${product.slug}`}>
-                                    <img src={product.image} alt={product.name} />
-                                </Link>
-                                <div className="product-info">
-                                    <Link to={`/product/${product.slug}`}>
-                                        <p>{product.name}</p>
-                                    </Link>
-                                    <p>
-                                        <strong>{product.price}</strong>
-                                    </p>
-                                    <button>Add to cart</button>
-                                </div>
-                            </div>
-                        ))}
-        </div>
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {/* mapping data from the data.js in the backend */}
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+               <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
     </div>
-    );
-};
+  );
+}
 
 export default HomeScreen;
